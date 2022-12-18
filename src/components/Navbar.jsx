3cbefd47adf25f02ac4b3c93a171135a1c69b889
Andreas Larssamils <larssamils.andreas@gmail.com>
2022-12-18 09:33:38 +0800

    updates

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <a href="/home">Homepage</a>
+        </li>
+        <li>
+          <a href="/about">About Us</a>
+        </li>
+        <li>
+          <a href="/blog">Blog</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
